82219ec085ddfecc7df3333e8add6dd87a1daf62
Daniyar <daniyar.turmukhambetov.10@ucl.ac.uk>
2016-10-07 17:37:35 +0100

    example 1 updated

--- example1.py
+++ example1.py
@@ -16,17 +16,15 @@ from scipy import ndimage
 import tensorflow as tf
 from stn import affine_transformer
 import numpy as np
-import matplotlib.pyplot as plt
+import scipy.misc
 
 # %% Create a batch of three images (1600 x 1200)
 # %% Image retrieved from:
 # %% https://raw.githubusercontent.com/skaae/transformer_network/master/cat.jpg
-im = ndimage.imread('cat.jpg')
+im = ndimage.imread('data/cat.jpg')
 im = im / 255.
 im = im.reshape(1, 1200, 1600, 3)
 im = im.astype('float32')
-
-plt.imshow(im[0])
 
 # %% Let the output size of the affine transformer be half the image size.
 out_size = (600, 800)
@@ -39,26 +37,29 @@ num_batch = 3
 x = tf.placeholder(tf.float32, [None, 1200, 1600, 3])
 x = tf.cast(batch, 'float32')
 
-# %% Create localisation network and convolutional layer
-with tf.variable_scope('spatial_transformer_0'):
-
-    # %% Create a fully-connected layer with 6 output nodes
-    n_fc = 6
-    W_fc1 = tf.Variable(tf.zeros([1200 * 1600 * 3, n_fc]), name='W_fc1')
-
-    # %% Zoom into the image
-    initial = np.array([[1.1, 0, 0], [0, 1.1, 0]])
-    initial = initial.astype('float32')
-    initial = initial.flatten()
-
-    b_fc1 = tf.Variable(initial_value=initial, name='b_fc1')
-    h_fc1 = tf.matmul(tf.zeros([num_batch, 1200 * 1600 * 3]), W_fc1) + b_fc1
-    h_trans = affine_transformer(x, h_fc1, out_size)
-
 # %% Run session
-sess = tf.Session()
-sess.run(tf.initialize_all_variables())
-y = sess.run(h_trans, feed_dict={x: batch})
+with tf.Session() as sess:
+  with tf.device("/cpu:0"):
+    # %% Create localisation network and convolutional layer
+    with tf.variable_scope('spatial_transformer_0'):
+    
+        # %% Create a fully-connected layer with 6 output nodes
+        n_fc = 6
+        W_fc1 = tf.Variable(tf.zeros([1200*1600*3, n_fc]), name='W_fc1')
+    
+        # %% Zoom into the image
+        initial = np.array([[0.5, 0, 0.0], [0, 0.5, 0.0]])
+        initial = initial.astype('float32')
+        initial = initial.flatten()
+    
+        b_fc1 = tf.Variable(initial_value=initial, name='b_fc1')
+        h_fc1 = tf.matmul(tf.zeros([num_batch, 1200*1600*3]), W_fc1) + b_fc1
+        h_trans = affine_transformer(x, h_fc1, out_size)
 
-#plt.imshow(y[0])
+    # %% Run session
+    sess.run(tf.initialize_all_variables())
+    y = sess.run(h_trans, feed_dict={x: batch})
+
+# save our result
+scipy.misc.imsave('example1_stn.png', y[0])
 

55c0495b68dfb9b46e7673a58751a1620e8da0c1
Daniyar <daniyar.turmukhambetov.10@ucl.ac.uk>
2016-10-11 20:52:57 +0100

    refactored classes

--- stn.py
+++ stn.py
@@ -18,10 +18,13 @@ class AffineTransformer(object):
 
   """
 
-  def __init__(self, name='SpatialAffineTransformer', **kwargs):
+  def __init__(self, U_shape, out_size, name='SpatialAffineTransformer', **kwargs):
     self.name = name
+    self.num_batch, self.height, self.width, self.num_channels = U_shape
+    self.out_size = out_size
+    self.grid = self._meshgrid()
   
-  def transform(self, U, theta, out_size):
+  def transform(self, U, theta):
     """
     Parameters
     ----------
@@ -43,137 +46,51 @@ class AffineTransformer(object):
 
     """
     with tf.variable_scope(self.name):
-        output = _affine_transform(theta, U, out_size)
+        output = self._affine_transform(theta, U)
     return output
 
+    def _meshgrid(self):
+        with tf.variable_scope('_meshgrid'):
+            # This should be equivalent to:
+            #  x_t, y_t = np.meshgrid(np.linspace(-1, 1, width),
+            #                         np.linspace(-1, 1, height))
+            #  ones = np.ones(np.prod(x_t.shape))
+            #  grid = np.vstack([x_t.flatten(), y_t.flatten(), ones])
+            x_t = tf.matmul(tf.ones(shape=tf.pack([self.out_size[0], 1])),
+                            tf.transpose(tf.expand_dims(tf.linspace(-1.0, 1.0, self.out_size[1]), 1), [1, 0]))
+            y_t = tf.matmul(tf.expand_dims(tf.linspace(-1.0, 1.0, self.out_size[0]), 1),
+                            tf.ones(shape=tf.pack([1, self.out_size[1]])))
+    
+            x_t_flat = tf.reshape(x_t, (1, -1))
+            y_t_flat = tf.reshape(y_t, (1, -1))
+    
+            ones = tf.ones_like(x_t_flat)
+            grid = tf.concat(0, [x_t_flat, y_t_flat, ones])
 
-def _repeat(x, n_repeats):
-    with tf.variable_scope('_repeat'):
-        rep = tf.transpose(
-            tf.expand_dims(tf.ones(shape=tf.pack([n_repeats, ])), 1), [1, 0])
-        rep = tf.cast(rep, 'int32')
-        x = tf.matmul(tf.reshape(x, (-1, 1)), rep)
-        return tf.reshape(x, [-1])
-
-def _interpolate(im, x, y, out_size):
-    with tf.variable_scope('_interpolate'):
-        # constants
-        num_batch = tf.shape(im)[0]
-        height = tf.shape(im)[1]
-        width = tf.shape(im)[2]
-        channels = tf.shape(im)[3]
-
-        max_y = tf.cast(tf.shape(im)[1] - 1, 'int32')
-        max_x = tf.cast(tf.shape(im)[2] - 1, 'int32')
-
-        x = tf.cast(x, 'float32')
-        y = tf.cast(y, 'float32')
-        height_f = tf.cast(height, 'float32')
-        width_f = tf.cast(width, 'float32')
-        out_height = out_size[0]
-        out_width = out_size[1]
-        zero = tf.zeros([], dtype='int32')
-
-        # scale indices from [-1, 1] to [0, width/height - 1]
-        x = tf.clip_by_value(x, -1, 1)
-        y = tf.clip_by_value(y, -1, 1)
-
-        x = (x + 1.0) / 2.0 * (width_f-1.001)
-        y = (y + 1.0) / 2.0 * (height_f-1.001)
-
-        # do sampling
-        x0 = tf.cast(tf.floor(x), 'int32')
-        x1 = x0 + 1
-        y0 = tf.cast(tf.floor(y), 'int32')
-        y1 = y0 + 1
-
-        x0 = tf.clip_by_value(x0, zero, max_x)
-        x1 = tf.clip_by_value(x1, zero, max_x)
-        y0 = tf.clip_by_value(y0, zero, max_y)
-        y1 = tf.clip_by_value(y1, zero, max_y)
-
-        dim2 = width
-        dim1 = width*height
-        base = _repeat(tf.range(num_batch)*dim1, out_height*out_width)
-        base_y0 = base + y0*dim2
-        base_y1 = base + y1*dim2
-        idx_a = base_y0 + x0
-        idx_b = base_y1 + x0
-        idx_c = base_y0 + x1
-        idx_d = base_y1 + x1
-
-        # use indices to lookup pixels in the flat image and restore
-        # channels dim
-        im_flat = tf.reshape(im, tf.pack([-1, channels]))
-        im_flat = tf.cast(im_flat, 'float32')
-        Ia = tf.gather(im_flat, idx_a)
-        Ib = tf.gather(im_flat, idx_b)
-        Ic = tf.gather(im_flat, idx_c)
-        Id = tf.gather(im_flat, idx_d)
-
-        # and finally calculate interpolated values
-        x0_f = tf.cast(x0, 'float32')
-        x1_f = tf.cast(x1, 'float32')
-        y0_f = tf.cast(y0, 'float32')
-        y1_f = tf.cast(y1, 'float32')
-        wa = tf.expand_dims(((x1_f-x) * (y1_f-y)), 1)
-        wb = tf.expand_dims(((x1_f-x) * (y-y0_f)), 1)
-        wc = tf.expand_dims(((x-x0_f) * (y1_f-y)), 1)
-        wd = tf.expand_dims(((x-x0_f) * (y-y0_f)), 1)
-        output = tf.add_n([wa*Ia, wb*Ib, wc*Ic, wd*Id])
-        return output
-
-def _meshgrid(height, width):
-    with tf.variable_scope('_meshgrid'):
-        # This should be equivalent to:
-        #  x_t, y_t = np.meshgrid(np.linspace(-1, 1, width),
-        #                         np.linspace(-1, 1, height))
-        #  ones = np.ones(np.prod(x_t.shape))
-        #  grid = np.vstack([x_t.flatten(), y_t.flatten(), ones])
-        x_t = tf.matmul(tf.ones(shape=tf.pack([height, 1])),
-                        tf.transpose(tf.expand_dims(tf.linspace(-1.0, 1.0, width), 1), [1, 0]))
-        y_t = tf.matmul(tf.expand_dims(tf.linspace(-1.0, 1.0, height), 1),
-                        tf.ones(shape=tf.pack([1, width])))
-
-        x_t_flat = tf.reshape(x_t, (1, -1))
-        y_t_flat = tf.reshape(y_t, (1, -1))
-
-        ones = tf.ones_like(x_t_flat)
-        grid = tf.concat(0, [x_t_flat, y_t_flat, ones])
-        return grid
-
-def _affine_transform(theta, input_dim, out_size):
-    with tf.variable_scope('_affine_transform'):
-        num_batch, height, width, num_channels = input_dim.get_shape().as_list()
-        theta = tf.reshape(theta, (-1, 2, 3))
-        theta = tf.cast(theta, 'float32')
-
-        # grid of (x_t, y_t, 1), eq (1) in ref [1]
-        height_f = tf.cast(height, 'float32')
-        width_f = tf.cast(width, 'float32')
-        out_height = out_size[0]
-        out_width = out_size[1]
-        grid = _meshgrid(out_height, out_width)
-        grid = tf.expand_dims(grid, 0)
-        grid = tf.reshape(grid, [-1])
-        grid = tf.tile(grid, tf.pack([num_batch]))
-        grid = tf.reshape(grid, [num_batch, 3, -1])
-
-        # Transform A x (x_t, y_t, 1)^T -> (x_s, y_s)
-        T_g = tf.batch_matmul(theta, grid)
-        x_s = tf.slice(T_g, [0, 0, 0], [-1, 1, -1])
-        y_s = tf.slice(T_g, [0, 1, 0], [-1, 1, -1])
-        x_s_flat = tf.reshape(x_s, [-1])
-        y_s_flat = tf.reshape(y_s, [-1])
-
-        input_transformed = _interpolate(
-            input_dim, x_s_flat, y_s_flat,
-            out_size)
-
-        output = tf.reshape(
-            input_transformed, tf.pack([num_batch, out_height, out_width, num_channels]))
-        return output
-
+            grid = tf.expand_dims(grid, 0)
+            grid = tf.reshape(grid, [-1])
+            grid = tf.tile(grid, tf.pack([self.num_batch]))
+            grid = tf.reshape(grid, [self.num_batch, 3, -1])
+            return grid
+    
+    def _affine_transform(self, theta, input_dim):
+        with tf.variable_scope('_affine_transform'):
+            theta = tf.reshape(theta, (-1, 2, 3))
+    
+            # Transform A x (x_t, y_t, 1)^T -> (x_s, y_s)
+            T_g = tf.batch_matmul(theta, self.grid)
+            x_s = tf.slice(T_g, [0, 0, 0], [-1, 1, -1])
+            y_s = tf.slice(T_g, [0, 1, 0], [-1, 1, -1])
+            x_s_flat = tf.reshape(x_s, [-1])
+            y_s_flat = tf.reshape(y_s, [-1])
+    
+            input_transformed = _interpolate(
+                input_dim, x_s_flat, y_s_flat,
+                self.out_size)
+    
+            output = tf.reshape(input_transformed, tf.pack([self.num_batch, self.out_size[0], self.out_size[1], self.num_channels]))
+            return output
+  
 
 class TPSTransformer(object):
     """Spatial Thin Plate Spline Transformer Layer
@@ -208,62 +125,55 @@ class TPSTransformer(object):
         out_size: tuple of two ints
             The size of the output of the spatial network (height, width)
         """
-        num_control_points = int(num_control_points)
-
-        self.right_mat, self.L_inv, self.source_points = _initialize_tps(U_shape, num_control_points, out_size)
-        self.name = name
+        self.num_control_points = int(num_control_points)
+        self.num_batch, self.height, self.width, self.num_channels = U_shape
         self.out_size = out_size
+        self.name = name
 
-    def transform(self, U, theta):
+        right_mat, self.L_inv, source_points = _initialize_tps(U_shape, self.num_control_points, self.out_size)
+
+        self.source_points = np.tile(source_points, [self.num_batch, 1, 1])
+        self.right_mat = tf.tile(tf.expand_dims(right_mat, 0), (self.num_batch, 1, 1))
+
+    def transform(self, U, theta, **kwargs):
         #The number of control points. Must be a
         #    perfect square. Points will be used to generate an evenly spaced grid.
 
         with tf.variable_scope(self.name):
-            output = _tps_transform(theta, U, self.out_size, self.right_mat, self.L_inv, self.source_points)
+            output = self._tps_transform(theta, U)
         return output
 
-def _tps_transform(dest_offsets, U, out_size, right_mat, L_inv, source_points):
-
-    num_batch, height, width, num_channels = U.get_shape().as_list()
-    num_control_points = source_points.shape[1]
-
-    out_height = out_size[0]
-    out_width = out_size[1]
-
-    # reshape destination offsets to be (num_batch, 2, num_control_points)
-    # and add to source_points
-    dest_points = np.tile(source_points, [num_batch, 1, 1]) + tf.reshape(
-            dest_offsets, tf.pack([num_batch, 2, num_control_points]))
-
-    # Solve as in ref [2]
-    dest_points = tf.reshape(dest_points, [num_batch*2, num_control_points])
-    coefficients = tf.matmul(dest_points, tf.transpose(L_inv[:, 3:]))
-    coefficients = tf.reshape(coefficients, [num_batch, 2, -1])
-
-    # Transform each point on the source grid (image_size x image_size)
-    right_mat = tf.tile(tf.expand_dims(right_mat, 0), (num_batch, 1, 1))
-    transformed_points = tf.batch_matmul(coefficients, right_mat)
-
-    # TODO this is ugly, but it works!
-    transformed_points = tf.reshape(transformed_points, [num_batch, 2, out_width, out_height])
-    transformed_points = tf.transpose(transformed_points, [0, 1, 3, 2])
-    transformed_points = tf.reshape(transformed_points, [num_batch, 2, out_height*out_width])
-
-    #transformed_points = tf.transpose(transformed_points, [0, 1, 2])
-    y_s_flat = tf.reshape(transformed_points[:,0,:], [-1])
-    x_s_flat = tf.reshape(transformed_points[:,1,:], [-1])
-
-    input_transformed = _interpolate(
-            U, x_s_flat, y_s_flat,
-            out_size)
+    def _tps_transform(self, theta, U):
+        out_height = self.out_size[0]
+        out_width = self.out_size[1]
     
-    #output = tf.reshape(input_transformed, 
-    #        tf.pack([num_batch, num_channels, out_height, out_width]))
-    #output = tf.transpose(output, [0, 2, 3, 1])
-    output = tf.reshape(input_transformed, 
-           tf.pack([num_batch, out_height, out_width, num_channels]))
-    #output = tf.transpose(output, [0, 2, 3, 1])
-    return output
+        # reshape destination offsets to be (num_batch, 2, num_control_points)
+        # and add to source_points
+        theta = self.source_points + tf.reshape(theta, tf.pack([self.num_batch, 2, self.num_control_points]))
+    
+        # Solve as in ref [2]
+        theta = tf.reshape(theta, [self.num_batch*2, self.num_control_points])
+        coefficients = tf.matmul(theta, tf.transpose(self.L_inv[:, 3:]))
+        coefficients = tf.reshape(coefficients, [self.num_batch, 2, -1])
+    
+        # Transform each point on the source grid (image_size x image_size)
+        transformed_points = tf.batch_matmul(coefficients, self.right_mat)
+    
+        # TODO this is ugly, but it works!
+        transformed_points = tf.reshape(transformed_points, [self.num_batch, 2, out_width, out_height])
+        transformed_points = tf.transpose(transformed_points, [0, 1, 3, 2])
+        transformed_points = tf.reshape(transformed_points, [self.num_batch, 2, out_height*out_width])
+    
+        #transformed_points = tf.transpose(transformed_points, [0, 1, 2])
+        y_s_flat = tf.reshape(transformed_points[:,0,:], [-1])
+        x_s_flat = tf.reshape(transformed_points[:,1,:], [-1])
+    
+        input_transformed = _interpolate(
+                U, x_s_flat, y_s_flat,
+                self.out_size)
+        
+        output = tf.reshape(input_transformed, tf.pack([self.num_batch, out_height, out_width, self.num_channels]))
+        return output
 
 
 def _U_func_numpy(x1, y1, x2, y2):
@@ -387,3 +297,80 @@ def _initialize_tps(U_shape, num_control_points, out_size):
     #source_points = tf.Variable(source_points, name='source_points')
 
     return right_mat, L_inv, source_points
+
+def _repeat(x, n_repeats):
+    with tf.variable_scope('_repeat'):
+        rep = tf.transpose(
+            tf.expand_dims(tf.ones(shape=tf.pack([n_repeats, ])), 1), [1, 0])
+        rep = tf.cast(rep, 'int32')
+        x = tf.matmul(tf.reshape(x, (-1, 1)), rep)
+        return tf.reshape(x, [-1])
+
+def _interpolate(im, x, y, out_size):
+    with tf.variable_scope('_interpolate'):
+        # constants
+        num_batch = tf.shape(im)[0]
+        height = tf.shape(im)[1]
+        width = tf.shape(im)[2]
+        channels = tf.shape(im)[3]
+
+        max_y = tf.cast(tf.shape(im)[1] - 1, 'int32')
+        max_x = tf.cast(tf.shape(im)[2] - 1, 'int32')
+
+        x = tf.cast(x, 'float32')
+        y = tf.cast(y, 'float32')
+        height_f = tf.cast(height, 'float32')
+        width_f = tf.cast(width, 'float32')
+        out_height = out_size[0]
+        out_width = out_size[1]
+        zero = tf.zeros([], dtype='int32')
+
+        # scale indices from [-1, 1] to [0, width/height - 1]
+        x = tf.clip_by_value(x, -1, 1)
+        y = tf.clip_by_value(y, -1, 1)
+
+        x = (x + 1.0) / 2.0 * (width_f-1.001)
+        y = (y + 1.0) / 2.0 * (height_f-1.001)
+
+        # do sampling
+        x0 = tf.cast(tf.floor(x), 'int32')
+        x1 = x0 + 1
+        y0 = tf.cast(tf.floor(y), 'int32')
+        y1 = y0 + 1
+
+        x0 = tf.clip_by_value(x0, zero, max_x)
+        x1 = tf.clip_by_value(x1, zero, max_x)
+        y0 = tf.clip_by_value(y0, zero, max_y)
+        y1 = tf.clip_by_value(y1, zero, max_y)
+
+        dim2 = width
+        dim1 = width*height
+        base = _repeat(tf.range(num_batch)*dim1, out_height*out_width)
+        base_y0 = base + y0*dim2
+        base_y1 = base + y1*dim2
+        idx_a = base_y0 + x0
+        idx_b = base_y1 + x0
+        idx_c = base_y0 + x1
+        idx_d = base_y1 + x1
+
+        # use indices to lookup pixels in the flat image and restore
+        # channels dim
+        im_flat = tf.reshape(im, tf.pack([-1, channels]))
+        im_flat = tf.cast(im_flat, 'float32')
+        Ia = tf.gather(im_flat, idx_a)
+        Ib = tf.gather(im_flat, idx_b)
+        Ic = tf.gather(im_flat, idx_c)
+        Id = tf.gather(im_flat, idx_d)
+
+        # and finally calculate interpolated values
+        x0_f = tf.cast(x0, 'float32')
+        x1_f = tf.cast(x1, 'float32')
+        y0_f = tf.cast(y0, 'float32')
+        y1_f = tf.cast(y1, 'float32')
+        wa = tf.expand_dims(((x1_f-x) * (y1_f-y)), 1)
+        wb = tf.expand_dims(((x1_f-x) * (y-y0_f)), 1)
+        wc = tf.expand_dims(((x-x0_f) * (y1_f-y)), 1)
+        wd = tf.expand_dims(((x-x0_f) * (y-y0_f)), 1)
+        output = tf.add_n([wa*Ia, wb*Ib, wc*Ic, wd*Id])
+        return output
+

7540aee2e6cff36ae606cc7b5efe63b8301ffab4
Daniyar Turmukhambetov <dant.kz@gmail.com>
2017-03-11 21:30:23 +0000

    undo 3d rotation

--- example_3daffine.py
+++ example_3daffine.py
@@ -11,9 +11,12 @@ with open('data/model.binvox', 'rb') as f:
     model = binvox_rw.read_as_3d_array(f)
 
 vol = model.data.copy().astype(np.float32)
+pad_size = 12
+vol = np.pad(vol, pad_width=[[pad_size,pad_size], [pad_size,pad_size], [pad_size,pad_size]], mode='constant')
+model.dims = (np.array(model.dims) + 2*pad_size).tolist()
 
 # input batch
-batch_size = 8
+batch_size = 5
 batch = np.expand_dims(vol, axis=3)
 batch = np.expand_dims(batch, axis=0)
 batch = np.tile(batch, [batch_size, 1, 1, 1, 1])
@@ -26,6 +29,7 @@ outsize = (int(vol.shape[0]), int(vol.shape[1]), int(vol.shape[2]))
 
 # Affine Transformation Layer
 stl = AffineVolumeTransformer(outsize)
+theta = tf.placeholder(tf.float32, [batch_size, stl.param_dim])
 
 # Identity transformation parameters
 initial = np.array([1.0, 0.0, 0.0, 0.0,
@@ -54,37 +58,71 @@ def transmat(phi, theta, psi, shiftmat=None):
     transmat = np.concatenate([rotmat, shiftmat],1)
     return transmat.flatten().astype(np.float32)
 
+def get_rot_diff(transmat_src, transmat_trg):
+    transmat_src = np.reshape(transmat_src, [3,4])
+    transmat_trg = np.reshape(transmat_trg, [3,4])
+    R_src = transmat_src[:,0:3]
+    t_src = transmat_src[:,3:4]
+    R_trg = transmat_trg[:,0:3]
+    t_trg = transmat_trg[:,3:4]
+    R_res = np.dot(R_src.T, R_trg)
+    t_res = -t_src + t_trg
+    result = np.concatenate([R_res, t_res], 1)
+    return result.flatten()
+
+
+cur_angles = 2*np.pi*(2*(np.random.rand(1, 3)-0.5))
+#cur_angles = np.zeros([1, 3])
+cur_theta1 = transmat(cur_angles[0,0], cur_angles[0,1], cur_angles[0,2])
 # Run session
 with tf.Session() as sess:
     with tf.device("/cpu:0"):
         with tf.variable_scope('spatial_transformer') as scope:
-            # Random jitter of the identity parameters
-            theta_ = np.zeros([batch_size, stl.param_dim], dtype=np.float32)
-            angle_step = 2*np.pi/batch_size
+            theta_canonical = np.tile(np.expand_dims(cur_theta1, axis=0), [batch_size, 1])
+
+            theta_random = np.zeros([batch_size, stl.param_dim], dtype=np.float32)
+            random_angles = 2*np.pi*(2*(np.random.rand(batch_size,3)-0.5))
             for i in xrange(batch_size):
-                theta_[i,:] = transmat(0, 0, i*angle_step)
-                #theta_[i,:] = transmat(0, 0, 0, np.array([0, 0, 0.5*i]))
-                #theta_[i,:] = transmat(0, 0, 0)
-            theta = tf.convert_to_tensor(theta_)
-            #theta = initial + 0.1*tf.random_normal([batch_size, stl.param_dim])
-            result = stl.transform(x, theta)
+                cur_theta = transmat(random_angles[i,0], random_angles[i,1], random_angles[i,2])
+                theta_random[i,:] = cur_theta
+
+
+            theta_inverse = np.zeros([batch_size, stl.param_dim], dtype=np.float32)
+            for i in xrange(batch_size):
+                cur_theta = get_rot_diff(theta_random[i,:], theta_canonical[i,:])
+                theta_inverse[i,:] = cur_theta
+
+            transformed = stl.transform(x, theta)
 
         sess.run(tf.global_variables_initializer())
         batch = np.transpose(batch, [0,3,2,1,4])
-        result_ = sess.run(result, feed_dict={x: batch})
-        result_ = np.transpose(result_, [0,3,2,1,4])
-        print(result_.shape)
+
+        x_canonical = sess.run(transformed, feed_dict={x: batch, theta: theta_canonical})
+        x_random = sess.run(transformed, feed_dict={x: batch, theta: theta_random})
+        x_inverse = sess.run(transformed, feed_dict={x: x_random, theta: theta_inverse})
+
+        x_canonical = np.transpose(x_canonical, [0,3,2,1,4])
+        x_random = np.transpose(x_random, [0,3,2,1,4])
+        x_inverse = np.transpose(x_inverse, [0,3,2,1,4])
 
 # save our result
-for i in range(result_.shape[0]):
-    cur_vol = result_[i,:,:,:,0]>0.5 # binary
+def save_binvox(cur_vol, name):
     cur_model = binvox_rw.Voxels(
             data=cur_vol, 
             dims=model.dims, 
             translate=model.translate, 
             scale=model.scale, 
             axis_order=model.axis_order)
-    with open('affine3d' + str(i) + '.binvox', 'wb') as f:
+    with open(name + '.binvox', 'wb') as f:
         cur_model.write(f)
+
+cur_vol = x_canonical[0,:,:,:,0]>0.5 # binary
+save_binvox(cur_vol, 'model_canonical')
+
+for i in range(batch_size):
+    cur_vol = x_random[i,:,:,:,0]>0.5 # binary
+    save_binvox(cur_vol, 'model_' + str(i) + 'random')
+    cur_vol = x_inverse[i,:,:,:,0]>0.5 # binary
+    save_binvox(cur_vol, 'model_' + str(i) + 'inverse')
 
 
